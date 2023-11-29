8d738c5d0c2e0f8653248fd50cfb39a58b520b5f
ramyag8 <143139987+ramyag8@users.noreply.github.com>
2023-11-29 15:53:27 -0600

    detail

--- models/wolf.js
+++ models/wolf.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose")
 const wolfSchema = mongoose.Schema({
 wolf_color: String,
 wolf_breed: String,
-wolf_price: Number
-})
+wolf_price: {type :Number,
+    min:5,
+    max:5000
+
+}
+
+});
 module.exports = mongoose.model("wolf",wolfSchema)
